93273a199ff02e25ba23e683266c509dfc9f9cd0
kartien <kevinramon523@gmail.com>
2023-03-08 12:09:01 -0500

    Add views and new fils sh

---
+++ dev.sh
@@ -0,0 +1,12 @@
+#1/bin/bash
+
+echo "  _   _            _       "          
+echo " ( ) ( )          ( )_ _    "         
+echo " | |/ /   _ _ _ __|  _)_)  __   ___  "
+echo " |   (  / _  )  __) | | |/ __ \  _  \ "
+echo " | |\ \( (_| | |  | |_| |  ___/ ( ) | "
+echo " (_) (_)\__ _)_)   \__)_)\____)_) (_) "
+
+
+python manage.py migrate 
+python manage.py runserver 3000
---
+++ run.sh
@@ -0,0 +1,5 @@
+#1/bin/bash
+
+python manage.py migrate 
+python manage.py runserver 3000
+
