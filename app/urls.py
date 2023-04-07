057c2179633333cb0dd8c957e99721b23e38e93b
kartien <kevinramon523@gmail.com>
2023-04-07 11:19:33 -0500

    Add new views and urls

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('inbox/', views.inbox, name="inbox")
 ]
